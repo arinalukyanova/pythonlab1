b2896ec21d745292d07abfbce7ce6c617f6ed30a
arinalukyanova <m2512665@edu.misis.ru>
2025-09-18 16:56:33 +0700

    Лабораторная работа 1

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ print("Привет,",name,"! Через год тебе будет",age+1,".")
 ![скриншот 1](/images/lab01/01_greeting.png)
 
 
+Задание 2
+```python
+n1=float(input("a: ").replace(",","."))
+n2=float(input("b: ").replace(",","."))
+sum1=n1+n2
+avg1=(n1+n2)/2
+print("sum=",round(sum1,2),";","avg=",round(avg1,2))
+```
+![скриншот 2](/images/lab01/02_sum_avg.png)
+
+
+Задание 3 
---
+++ src/lab01/03_discount_vat.py
@@ -0,0 +1,9 @@
+price=float(input("цена: ").replace(",","."))
+discount=float(input("скидка: ").replace(",","."))
+vat=float(input("НДС: ").replace(",","."))
+base = price * (1 - discount/100)
+vat_amount = base * (vat/100)
+total = base + vat_amount
+print("База после скидки:",round(base,2),"P")
+print("НДС:",round(vat_amount,2),"P")
+print("Итого к оплате:",round(total,2),"P")
