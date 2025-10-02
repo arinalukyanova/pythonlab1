e4e1388fda183cc398f2fbf05b84638a08c0ac34
arinalukyanova <m2512665@edu.misis.ru>
2025-10-02 18:05:23 +0700

    Лабораторная работа 2

--- src/lab02/tuples.py
+++ src/lab02/tuples.py
@@ -6,20 +6,22 @@ def student_registration(fio: str, group: str, gpa: float) -> tuple[str, str, fl
     if type(group) is not str:
         raise TypeError('Группа студента должна быть в формате строки')
     if type(gpa) is not float:
-        raise TypeError('Группа должна быть в формате float')
+        raise TypeError('GPA должен быть в формате float')
     return tuple([fio, group, gpa])
-
 def format_record(rec: tuple[str, str, float]) -> str:
     if type(rec) is not tuple:
         raise TypeError('Ввод в неправильном формате')
     else:
-        fio = list(rec[0].split())
-        if len(fio) == 3:
-            return f'{fio[0][0].upper() + fio[0][1:]} {fio[1][0].upper()}.{fio[2][0].upper()}., гр. {group}, GPA {round(rec[2], 2)}'
+        fio_parts = rec[0].split()
+        group = rec[1]
+        gpa = rec[2]
+        if len(fio_parts) == 3:
+            return f'{fio_parts[0].capitalize()} {fio_parts[1][0].upper()}.{fio_parts[2][0].upper()}., гр. {group}, GPA {round(gpa, 2)}'
         else:
-            return f'{fio[0][0].upper() + fio[0][1:]} {fio[1][0].upper()}., гр. {group}, GPA {round(rec[2], 2)}'
-fio=input()
-group=input()
-gpa=float(input())
-rec=student_registration(fio,group,gpa)
+            return f'{fio_parts[0].capitalize()} {fio_parts[1][0].upper()}., гр. {group}, GPA {round(gpa, 2)}'
+data = input().split()
+fio = " ".join(data[:-2])
+group = data[-2]
+gpa = float(data[-1])
+rec = student_registration(fio, group, gpa)
 print(format_record(rec))
